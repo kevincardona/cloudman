451018c6c1cd9ce94412dffc736ec12133b9b906
kevincardona <kevincardona1@gmail.com>
2017-12-21 23:50:59 -0500

    finished json web tokenizer + started retrieving files

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from gevent import monkey
 monkey.patch_all()
 
+import jwt
+import json
 import sys
 import requests
 import server_config
 
-from flask import Flask, request
+from flask import Flask, request, send_file
 from gevent import wsgi
 
 sys.path.append('routes/')
@@ -13,12 +15,13 @@ from auth import asignup, alogin, authenticate
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=['GET', 'POST'])
 def index ():
     test()
     return "Connected to " + server_config.SERVER_IP + "!"
 
+
+# TODO: Limit username/password character use + create user storage folder
 @app.route("/signup", methods=['POST'])
 def signup():
     username = request.form['username']
@@ -30,6 +33,21 @@ def login():
     username = request.form['username']
     password = request.form['password']
     return alogin(username, password)
+
+@app.route("/file", methods=['POST'])
+def files():
+    token = request.form['token']
+    if (authenticate(token) == False):
+        res = { "success": False, "message": "Invalid token" }
+        return json.dumps(res)
+
+    decoded = jwt.decode(token, server_config.JWT_SECRET, server_config.JWT_ALGORITHM )
+
+    user_dir = "storage/" + decoded['username'] + "/"
+    print user_dir
+    filename = user_dir + request.form['filename']
+    return send_file(filename, mimetype='image/png')
+
 
 
 
--- routes/auth.py
+++ routes/auth.py
@@ -1,13 +1,16 @@
+import jwt
 import json
 import bcrypt
 import server_config
+from datetime import datetime, timedelta
+
 
 def asignup(username, password):
     file = json.load(open('users.json'))
     for user in file["users"]:
         if (user['username'] == username):
-            return "Username already exists"
-
+            res = { "success": False, "message": "Username already exists!" }
+            return json.dumps(res)
     hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     newuser = {
         'username': username,
@@ -17,7 +20,8 @@ def asignup(username, password):
     data = file
     with open('users.json', 'w') as outfile:
         json.dump(data, outfile)
-    return "Signed up"
+    res = { "success": True, "message": "Successfully signed up!" }
+    return json.dumps(res)
 
 
 def alogin(username, password):
@@ -26,8 +30,21 @@ def alogin(username, password):
         if (user['username'] == username):
             upass = user['password']
             if (bcrypt.hashpw(password.encode('utf-8'), upass.encode('utf-8'))) == user['password']:
-                return "Logged In"
-    return "Failed to log in"
+                token_items = {
+                    'username': user['username'],
+                    'exp': datetime.utcnow() + timedelta(seconds=server_config.JWT_EXP_SECONDS)
+                }
+                token = jwt.encode(token_items, server_config.JWT_SECRET, server_config.JWT_ALGORITHM)
+                res = { "success": True, "message": "Successfully logged in!", "token": token }
+                return json.dumps(res)
+    res = { "success": False, "message": "Invalid login information" }
+    return json.dumps(res)
 
-def authenticate():
-    return "Authenticated"
+def authenticate(token):
+    try:
+        decoded = jwt.decode(token, server_config.JWT_SECRET, server_config.JWT_ALGORITHM )
+        if (decoded):
+            return True
+        return False
+    except Exception:
+        return False
