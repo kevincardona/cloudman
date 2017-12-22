dc71db1ac09df005afd153e44468099aa1c61cd4
kevincardona <kevincardona1@gmail.com>
2017-12-22 00:08:02 -0500

    added auth middleware :)

--- app.py
+++ app.py
@@ -9,11 +9,29 @@ import server_config
 
 from flask import Flask, request, send_file
 from gevent import wsgi
+from functools import wraps
 
 sys.path.append('routes/')
 from auth import asignup, alogin, authenticate
 
 app = Flask(__name__)
+
+def requires_auth(request):
+    token = request.form['token']
+    if (authenticate(token) == False):
+        res = { "success": False, "message": "Invalid token" }
+        return json.dumps(res)
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.form['token']
+        if (authenticate(token) == False):
+            res = { "success": False, "message": "Invalid token" }
+            return json.dumps(res)
+        return f(*args, **kwargs)
+    return decorated
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index ():
@@ -35,20 +53,23 @@ def login():
     return alogin(username, password)
 
 @app.route("/file", methods=['POST'])
+@requires_auth
 def files():
     token = request.form['token']
-    if (authenticate(token) == False):
-        res = { "success": False, "message": "Invalid token" }
-        return json.dumps(res)
-
     decoded = jwt.decode(token, server_config.JWT_SECRET, server_config.JWT_ALGORITHM )
-
     user_dir = "storage/" + decoded['username'] + "/"
-    print user_dir
+
     filename = user_dir + request.form['filename']
-    return send_file(filename, mimetype='image/png')
+    return send_file(filename)
+
+@app.route("/ls", methods=['POST'])
+def ls():
+    print "ls"
 
 
+@app.route("/mkdir", methods=['POST'])
+def mkdir():
+    print "mkdir"
 
 
 server = wsgi.WSGIServer((server_config.SERVER_IP, server_config.SERVER_PORT), app)
