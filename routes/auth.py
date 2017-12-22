0abf765bfa588a87186fe1a64457eb8572ef8996
kevincardona <kevincardona1@gmail.com>
2017-12-21 19:55:00 -0500

    added bcrypt for password encrpytion!

--- routes/auth.py
+++ routes/auth.py
@@ -1,13 +1,17 @@
 import json
+import bcrypt
+import server_config
 
 def asignup(username, password):
     file = json.load(open('users.json'))
     for user in file["users"]:
         if (user['username'] == username):
             return "Username already exists"
+
+    hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     newuser = {
         'username': username,
-        'password': password
+        'password': hash
     }
     file["users"].append(newuser)
     data = file
@@ -19,8 +23,10 @@ def asignup(username, password):
 def alogin(username, password):
     file = json.load(open('users.json'))
     for user in file["users"]:
-        if (user['username'] == username) & (user['password'] == password):
-            return "Logged In"
+        if (user['username'] == username):
+            upass = user['password']
+            if (bcrypt.hashpw(password.encode('utf-8'), upass.encode('utf-8'))) == user['password']:
+                return "Logged In"
     return "Failed to log in"
 
 def authenticate():
